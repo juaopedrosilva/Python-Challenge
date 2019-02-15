dfa91ed6920c25a6c9fbb11c9fdb2751879d9f1b
João Pedro Silva <pedrojoaoti@gmail.com>
2019-02-15 14:53:03 -0300

    day 1

---
+++ EstruturaSequencial/atividade6.py
@@ -0,0 +1,4 @@
+pi = 3.14
+r =  float(input("Qual é o raio: "))
+area = float(pi * (r**2))
+print(str(area))
---
+++ EstruturaSequencial/atividade7.py
@@ -0,0 +1,4 @@
+l = int(17)
+area =  float(l * l)
+dobro =  2 * float(area)
+print("o dobro desta área é ", dobro)
